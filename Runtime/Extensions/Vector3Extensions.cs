ba20540549b4cc067ab9818281b49f768dcf021d
pickgliss <pickgliss@gmail.com>
2024-06-17 09:41:59 +0800

    RandomPointInAnnulus add

--- Runtime/Extensions/Vector3Extensions.cs
+++ Runtime/Extensions/Vector3Extensions.cs
@@ -48,5 +48,26 @@ public static class Vector3Extensions {
             v1.x != 0 ? v0.x / v1.x : v0.x, 
             v1.y != 0 ? v0.y / v1.y : v0.y, 
             v1.z != 0 ? v0.z / v1.z : v0.z);  
-    }        
+    }
+    /// <summary>
+    /// Computes a random point in an annulus (a ring-shaped area) based on minimum and 
+    /// maximum radius values around a central Vector3 point (origin).
+    /// </summary>
+    /// <param name="origin">The center Vector3 point of the annulus.</param>
+    /// <param name="minRadius">Minimum radius of the annulus.</param>
+    /// <param name="maxRadius">Maximum radius of the annulus.</param>
+    /// <returns>A random Vector3 point within the specified annulus.</returns>
+    public static Vector3 RandomPointInAnnulus(this Vector3 origin, float minRadius, float maxRadius) {
+        float angle = Random.value * Mathf.PI * 2f;
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    
+        // Squaring and then square-rooting radii to ensure uniform distribution within the annulus
+        float minRadiusSquared = minRadius * minRadius;
+        float maxRadiusSquared = maxRadius * maxRadius;
+        float distance = Mathf.Sqrt(Random.value * (maxRadiusSquared - minRadiusSquared) + minRadiusSquared);
+    
+        // Converting the 2D direction vector to a 3D position vector
+        Vector3 position = new Vector3(direction.x, 0, direction.y) * distance;
+        return origin + position;
+    }
 }
