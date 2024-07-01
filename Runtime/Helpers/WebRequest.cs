ad97bf89fd5caa57ff6f2ce21a5fc4b543964931
pickgliss <pickgliss@gmail.com>
2024-07-01 08:02:45 +0800

    webrequest remove override

--- Runtime/Helpers/WebRequest.cs
+++ Runtime/Helpers/WebRequest.cs
@@ -7,60 +7,44 @@ namespace UnityUtils
 {
     public static class WebRequest
     {
-        public static UnityWebRequestAsyncOperation Get(string url)
+        public static UnityWebRequest Get(string url)
         {
             var request = UnityWebRequest.Get(url);
-            return request.SendWebRequest();
+            return request;
         }
 
-        public static UnityWebRequestAsyncOperation Post(string url, string body)
+        public static UnityWebRequest Post(string url, string body)
         {
             var request = new UnityWebRequest(url, "POST");
             request.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(body));
             request.downloadHandler = new DownloadHandlerBuffer();
             request.SetRequestHeader("Content-Type", "application/json");
-            return request.SendWebRequest();
+            return request;
         }
 
-        public static async void WithCallback(this UnityWebRequestAsyncOperation operation, Action<string> callback)
+        public static async Task<string> Send(this UnityWebRequest request)
         {
-            await operation.AsTask();
-            if (operation.webRequest.result == UnityWebRequest.Result.Success)
+            await request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
             {
-                callback?.Invoke(operation.webRequest.downloadHandler.text);
+                return request.downloadHandler.text;
             }
-            else
-            {
-                var url = operation.webRequest.url;
-                Debug.LogError($"{url}  error: {operation.webRequest.error}");
-            }
+            Debug.LogError($"{request.url}  error: {request.error}");
+            return null;
         }
 
-        public static async Task<T> WithSerializer<T>(this UnityWebRequestAsyncOperation operation,
+        public static async Task<T> Send<T>(this UnityWebRequest request,
             ISerializer serializer = null)
         {
-            var s = serializer ?? ISerializer.Json;
-            await operation.AsTask();
-            if (operation.webRequest.result == UnityWebRequest.Result.Success)
+            await request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
             {
-                return s.Deserialize<T>(operation.webRequest.downloadHandler.text);
+                var s = serializer ?? ISerializer.Json;
+                return s.Deserialize<T>(request.downloadHandler.text);
             }
-            var url = operation.webRequest.url;
-            Debug.LogError($"{url}  error: {operation.webRequest.error}");
+            var url = request.url;
+            Debug.LogError($"{url}  error: {request.error}");
             return default;
-        }
-        public static async void WithOverwrite<T>(this UnityWebRequestAsyncOperation operation, T obj)
-        {
-            await operation.AsTask();
-            if (operation.webRequest.result == UnityWebRequest.Result.Success)
-            {
-                JsonUtility.FromJsonOverwrite(operation.webRequest.downloadHandler.text, obj);
-            }
-            else
-            {
-                var url = operation.webRequest.url;
-                Debug.LogError($"{url}  error: {operation.webRequest.error}");
-            }
         }
     }
 }
